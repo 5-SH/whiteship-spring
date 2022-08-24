9218909d7ad532fa4916d4f220374784001f32b5
OSH <llll_lll_lll@naver.com>
2022-08-24 20:49:38 +0900

    스프링 logging

--- springapp1/src/main/java/com/sh/springapp1/SampleRunner2.java
+++ springapp1/src/main/java/com/sh/springapp1/SampleRunner2.java
@@ -1,5 +1,7 @@
 package com.sh.springapp1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,6 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner2 implements ApplicationRunner {
+
+  private Logger logger = LoggerFactory.getLogger(SampleRunner2.class);
+
   @Autowired
   private String hello;
 
@@ -15,10 +20,16 @@ public class SampleRunner2 implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    System.out.println("=======================");
-    System.out.println(hello);
-    System.out.println(shProperties.getName());
-    System.out.println(shProperties.getFullName());
-    System.out.println("=======================");
+//    System.out.println("=======================");
+//    System.out.println(hello);
+//    System.out.println(shProperties.getName());
+//    System.out.println(shProperties.getFullName());
+//    System.out.println("=======================");
+
+    logger.debug("=======================");
+    logger.debug(hello);
+    logger.debug(shProperties.getName());
+    logger.debug(shProperties.getFullName());
+    logger.debug("=======================");
   }
 }
