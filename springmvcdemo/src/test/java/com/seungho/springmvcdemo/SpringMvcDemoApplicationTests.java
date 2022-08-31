e1d3c8ed4cd69671ad285438fb8c29064e7ef442
OSH <llll_lll_lll@naver.com>
2022-08-31 22:43:38 +0900

    webMVC - HttpMessageConverters

---
+++ springmvcdemo/src/test/java/com/seungho/springmvcdemo/SpringMvcDemoApplicationTests.java
@@ -0,0 +1,4 @@
+package com.seungho.springmvcdemo;
+
+public class SpringMvcDemoApplicationTests {
+}
---
+++ springmvcdemo/src/test/java/com/seungho/springmvcdemo/user/UserControllerTest.java
@@ -0,0 +1,42 @@
+package com.seungho.springmvcdemo.user;
+
+import org.hamcrest.EasyMock2Matchers;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void hello() throws Exception {
+    mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("hello"));
+  }
+
+  @Test
+  public void createUser() throws Exception {
+    String userJson = "{\"username\": \"seungho\", \"password\": \"123456\"}";
+    mockMvc.perform(post("/users/create")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .content(userJson))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.username", Is.is("seungho")))
+            .andExpect(jsonPath("$.password", Is.is("123456")));
+  }
+}
