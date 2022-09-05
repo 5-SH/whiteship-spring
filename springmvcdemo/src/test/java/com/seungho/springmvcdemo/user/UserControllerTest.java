872e5fa4f4737f700fb288e35c219a3117e0de6e
OSH <llll_lll_lll@naver.com>
2022-09-05 20:43:58 +0900

    springboot 정적 리소스, webjars, favicon

--- springmvcdemo/src/test/java/com/seungho/springmvcdemo/user/UserControllerTest.java
+++ springmvcdemo/src/test/java/com/seungho/springmvcdemo/user/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.seungho.springmvcdemo.user;
 
-import org.hamcrest.EasyMock2Matchers;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,5 +37,17 @@ public class UserControllerTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.username", Is.is("seungho")))
             .andExpect(jsonPath("$.password", Is.is("123456")));
+  }
+
+  @Test
+  public void createUser_XML() throws Exception {
+    String userJson = "{\"username\": \"seungho\", \"password\": \"123456\"}";
+    mockMvc.perform(post("/users/create")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_XML)
+                    .content(userJson))
+            .andExpect(status().isOk())
+            .andExpect(xpath("/User/username").string("seungho"))
+            .andExpect(xpath("/User/password").string("123456"));
   }
 }
