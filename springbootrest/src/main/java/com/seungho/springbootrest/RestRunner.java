2b9a44a9ed53e35289c32bfabec8c577f50bfbb1
5-SH <llll_lll_lll@naver.com>
2022-12-08 07:23:09 +0900

    RestTemplate 와 WebClient 커스터마이징

--- springbootrest/src/main/java/com/seungho/springbootrest/RestRunner.java
+++ springbootrest/src/main/java/com/seungho/springbootrest/RestRunner.java
@@ -24,6 +24,33 @@ public class RestRunner implements ApplicationRunner {
 
     // Sync blocking
 //    stopwatch.start();
+    /**
+     * RestTemplate 는 Thread-Safe 하다.
+     * 따라서 하나의 RestTemplate 를 여러 곳에서 동시에 사용해도 된다.
+     * RestTempalte 는 http 요청을 보낼 때 디폴트로 SimpleClientHttpRequestFactory 를 활용해 처리한다.
+     * SimpleClinetHttpRequestFactory 는 요청마다 Connectiom 을 만들고
+     * 많은 기능이 빠져 있어 실제 프로젝트에서 사용하기에 무리가 있다.
+     * Connection pool 같은 기능을 추가하고 싶으면 HttpComponentsClientHttpRequestFactory 를 사용해야 한다.
+     *
+     * RestTemplate 가 보통 커스터마이징 되어 사용되기 때문에
+     * Springboot 에서는 자동 설정된  RestTemplate 를 제공하지 않는다.
+     * 대신 자동 정된 Springboot 는 RestTemplateBuilder 를 제공해 필요할 때
+     * HttpMessageConverters 등이 자동 설정된 RestTemplate 를 받을 수 있다.
+     *
+     * RestTemplate 를 커스터마이징 하는 방법은 3가지가 있는데,
+     * 커스터마이징한 내용을 어떤 범위로 적용하려는지에 따라 선택할 수 있다.
+     *
+     * 1) 최대한 지역적으로 사용하고 싶으면 RestTemplateBuilder 를 메서드에 주입받고
+     * 다시 커스터마이징한 후 필요에 따라 메서드를 호출한다. 각 메서드 호출은 새 RestTemplateBuilder
+     * 인스턴스를 반환하므로 커스터마이징은 이 빌더를 사용할 때만 적용된다.
+     *
+     * 2) 애플리케이션 전체에 전역적으로 사용하고 싶으면 RestTemplateCustomizer 를 사용한다.
+     * RestTemplateCustomizer 에 설정한 내용은 모든 RestTemplateBuilder 에 적용된다.
+     *
+     * 3) 커스터마이징 된 RestTemplateBuilder 빈을 새로 만든다.
+     * RestTemplateCustomizer 에 커스터마이징한 내용을 새로 만든 RestTemplateBuilder 에
+     * 적용하고 싶으면, RestTemplateBuilderConfigurer 를 활용해 커스터마이징 한다.
+     */
 //    RestTemplate restTemplate = restTemplateBuilder.build();
 //    String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
 //    System.out.println(helloResult);
@@ -33,7 +60,10 @@ public class RestRunner implements ApplicationRunner {
 //    System.out.println(stopwatch.prettyPrint());
 
     // Async non-blocking
-    WebClient webClient = builder.build();
+    WebClient webClient = builder
+            // 지역적으로 적용
+//            .baseUrl("http://localhost:8080")
+            .build();
 
     /**
      * webClient 첫 요청은 1s 정도 더 걸림
@@ -41,7 +71,10 @@ public class RestRunner implements ApplicationRunner {
      * init 요청 없이 바로 hello, world 요청을 보내면
      * world 응답에 4s 걸림
      */
-    webClient.get().uri("http://localhost:8080/init")
+    webClient.get()
+//            .uri("http://localhost:8080/init")
+            // 커스터마이징 설정 적용 후
+            .uri("init")
             .retrieve()
             .bodyToMono(String.class)
             .subscribe(s -> {
@@ -50,7 +83,10 @@ public class RestRunner implements ApplicationRunner {
               stopwatch.start();
             });
 
-    webClient.get().uri("http://localhost:8080/hello")
+    webClient.get()
+//            .uri("http://localhost:8080/hello")
+            // 커스터마이징 설정 적용 후
+            .uri("hello")
             .retrieve()
             .bodyToMono(String.class)
             .subscribe(s -> {
@@ -64,7 +100,10 @@ public class RestRunner implements ApplicationRunner {
               stopwatch.start();
             });
 
-    webClient.get().uri("http://localhost:8080/world")
+    webClient.get()
+//            .uri("http://localhost:8080/world")
+            // 커스터마이징 설정 적용 후
+            .uri("world")
             .retrieve()
             .bodyToMono(String.class)
             .subscribe(s -> {
--- springbootrest/src/main/java/com/seungho/springbootrest/SpringbootrestApplication.java
+++ springbootrest/src/main/java/com/seungho/springbootrest/SpringbootrestApplication.java
@@ -2,6 +2,12 @@ package com.seungho.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class SpringbootrestApplication {
@@ -10,4 +16,25 @@ public class SpringbootrestApplication {
 		SpringApplication.run(SpringbootrestApplication.class, args);
 	}
 
+	// 전역적으로 적용
+	@Bean
+	public WebClientCustomizer webClientCustomizer() {
+		return new WebClientCustomizer() {
+			@Override
+			public void customize(WebClient.Builder webClientBuilder) {
+				webClientBuilder.baseUrl("http://localhost:8080");
+			}
+		};
+	}
+
+	// RestTemplate 에서 Apache HttpClient 를 사용하도록 수정
+	@Bean
+	public RestTemplateCustomizer restTemplateCustomizer() {
+		return new RestTemplateCustomizer() {
+			@Override
+			public void customize(RestTemplate restTemplate) {
+				restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+			}
+		};
+	}
 }
